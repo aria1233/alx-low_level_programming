bdf206728572e604aa98ddef2ca9020180689495
aria1233 <andreawacu123@gmail.com>
2022-09-16 19:59:36 -0700

    h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *
+ * print_line - print lines
+ *
+ * @n: number of lines
+ *
+ * Return: (0)
+ *
+ */
+
+void print_line(int n)
+
+{
+
+int i;
+
+if (n <= 0)
+
+{
+
+_putchar('\n');
+
+}
+
+else
+
+{
+
+for (i = 0; i < n; i++)
+
+{
+
+_putchar('_');
+
+}
+
+_putchar('\n');
+
+}
+
+}
