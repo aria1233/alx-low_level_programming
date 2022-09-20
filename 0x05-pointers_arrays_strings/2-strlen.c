2f528ff3b2686f091a312f031de540edabda12ed
aria1233 <andreawacu123@gmail.com>
2022-09-20 07:22:42 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _strlen - returns length of string
+ * @s: number being tested
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	s = "string";
+	
+	len = _strlen(char *s);
+	printf("_strlen(char *s): %d\n", len);
+	return (0);
+}
